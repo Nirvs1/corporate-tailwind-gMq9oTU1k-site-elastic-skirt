d960b96816c3c42fa40bdc37e81f0fa38613c7d0
Nirvan <93690524+Nirvs1@users.noreply.github.com>
2022-07-25 21:27:47 +0100

    First commit

---
+++ pages/about.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { getProps } from "@builtjs/site";
+import Error from "next/error";
+import { withRouter } from "next/router";
+import SEO from "@/templates/head/seo/seo";
+import Cover4 from "@/templates/covers/cover-4/cover-4";
+import Layout from "@/layout/layout";
+
+const Page = (props) => {
+  if (props.error) {
+    return <Error statusCode={props.error.code} />
+  }
+  return (
+     <>
+	<SEO content={props.aboutSeoContent} />
+		<Layout headerContent={props.headerContent}  footerContent={props.footerContent} >
+			 <Cover4 content={props.aboutLandingContent} />
+			</Layout>
+		</>
+  );
+};
+
+export default withRouter(Page);
+
+export async function getStaticProps() {
+  let props = await getProps({pageSlug: 'about'});
+  return {
+    props: props,
+    revalidate: 10
+  };
+}
---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { getProps } from "@builtjs/site";
+import Error from "next/error";
+import { withRouter } from "next/router";
+import SEO from "@/templates/head/seo/seo";
+import Block2 from "@/templates/blocks/block-2/block-2";
+import Cover5 from "@/templates/covers/cover-5/cover-5";
+import Layout from "@/layout/layout";
+
+const Page = (props) => {
+  if (props.error) {
+    return <Error statusCode={props.error.code} />
+  }
+  return (
+     <>
+	<SEO content={props.homeSeoContent} />
+		<Layout headerContent={props.headerContent}  footerContent={props.footerContent} >
+			 <Cover5 content={props.mainLandingContent} />
+			 <Block2 content={props.aboutTeaserContent} />
+			</Layout>
+		</>
+  );
+};
+
+export default withRouter(Page);
+
+export async function getStaticProps() {
+  let props = await getProps({pageSlug: 'home'});
+  return {
+    props: props,
+    revalidate: 10
+  };
+}
